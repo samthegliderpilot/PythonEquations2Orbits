df65630ea3e0fbaf806fa70efb3260749e1a4b1f
Sam <samthegliderpilot@gmail.com>
2022-08-14 15:32:05 -0400

    Playing with graphics, getting a little further with equinoctial elements

--- pyeq2orb/Coordinates/EquinoctialElements.py
+++ pyeq2orb/Coordinates/EquinoctialElements.py
@@ -1,72 +1,75 @@
-# import sympy as sy
-# import math
-# from KeplerianModule import KeplerianElements
-# from pyeq2orb.Coordinates.CartesianModule import Cartesian, MotionCartesian
-# import sympy as sy
-# class EquinoctialElements:
-#     def __init__(self, a, h, k, p, q, f, mu) :
-#         self.SemiMajorAxis = a
-#         self.EccentricitySinTermH = h
-#         self.EccentricityCosTermK = k
-#         self.InclinationSinTermP = p
-#         self.InclinationCosTermQ = q
-#         self.TrueLongitude = f
-#         self.GravitationalParameter = mu
+import sympy as sy
+import math
+from KeplerianModule import KeplerianElements
+from pyeq2orb.Coordinates.CartesianModule import Cartesian, MotionCartesian
+import sympy as sy
+class EquinoctialElements:
+    def __init__(self, a, h, k, p, q, f, mu) :
+        self.SemiMajorAxis = a
+        self.EccentricitySinTermH = h
+        self.EccentricityCosTermK = k
+        self.InclinationSinTermP = p
+        self.InclinationCosTermQ = q
+        self.TrueLongitude = f
+        self.GravitationalParameter = mu
     
-#     def ToKeplerian(self) -> KeplerianElements :
-#         a = self.SemiMajorAxis
-#         h = self.EccentricitySinTermH
-#         k = self.EccentricityCosTermK
-#         p = self.InclinationSinTermP
-#         q = self.InclinationCosTermQ
-#         ta = self.TrueLongitude
+    def ToKeplerian(self) -> KeplerianElements :
+        a = self.SemiMajorAxis
+        h = self.EccentricitySinTermH
+        k = self.EccentricityCosTermK
+        p = self.InclinationSinTermP
+        q = self.InclinationCosTermQ
+        ta = self.TrueLongitude
 
-#         e = (h**2+k**2)**(1.0/2.0)
-#         i = 2*sy.atan(p**2+q**2)**(1.0/2.0)
-#         raan = sy.atan(p/q)
-#         w = sy.atan(h/k)-sy.atan(p/q)
-#         ta = self.TrueLongitude - w - raan
+        e = (h**2+k**2)**(1.0/2.0)
+        i = 2*sy.atan(p**2+q**2)**(1.0/2.0)
+        raan = sy.atan(p/q)
+        w = sy.atan(h/k)-sy.atan(p/q)
+        ta = self.TrueLongitude - w - raan
 
-#         return KeplerianElements(a, e, i, w, raan, ta, self.GravitationalParameter)
+        return KeplerianElements(a, e, i, w, raan, ta, self.GravitationalParameter)
 
-#     def ToMotionCartesian(self) -> MotionCartesian :
-#         cL = sy.cos(self.TrueLongitude)
-#         sL = sy.sin(self.TrueLongitude)
-#         raise Exception("Incomplete")
+    def ToMotionCartesian(self) -> MotionCartesian :
+        return self.ToKeplerian().ToInertialMotionCartesian() # TODO: something that avoids keplerian elements
 
-# def ConvertKeplerianToEquinoctial(keplerianElements : KeplerianElements) ->EquinoctialElements :
-#     a = keplerianElements.SemiMajorAxis
-#     e = keplerianElements.Eccentricity
-#     i = keplerianElements.Inclination
-#     w = keplerianElements.ArgumentOfPeriapsis
-#     raan = keplerianElements.RightAscensionOfAscendingNode
-#     ta = keplerianElements.TrueAnomaly
+    @staticmethod
+    def FromMotionCartesian(motion, gravitationalParameter) :
+        # TODO: something that avoids keplerian elements
+        return ConvertKeplerianToEquinoctial(KeplerianElements.FromMotionCartesian(motion, gravitationalParameter))
 
-#     h = e*sy.sin(w+raan)
-#     k = e*sy.cos(w+raan)
-#     p = sy.tan(i/w)*sy.sin(raan)
-#     q = sy.tan(i/w)*sy.cos(raan)
-#     f = w+raan+ta
+def ConvertKeplerianToEquinoctial(keplerianElements : KeplerianElements) ->EquinoctialElements :
+    a = keplerianElements.SemiMajorAxis
+    e = keplerianElements.Eccentricity
+    i = keplerianElements.Inclination
+    w = keplerianElements.ArgumentOfPeriapsis
+    raan = keplerianElements.RightAscensionOfAscendingNode
+    ta = keplerianElements.TrueAnomaly
 
-#     return EquinoctialElements(a, h, k, p, q, f, keplerianElements.GravitationalParameter)
+    h = e*sy.sin(w+raan)
+    k = e*sy.cos(w+raan)
+    p = sy.tan(i/w)*sy.sin(raan)
+    q = sy.tan(i/w)*sy.cos(raan)
+    f = w+raan+ta
+
+    return EquinoctialElements(a, h, k, p, q, f, keplerianElements.GravitationalParameter)
 
 
-# def CreateSymbolicElements(elementOf = None) -> EquinoctialElements :
-#     if(elementOf == None):
-#         a = sy.Symbol('a', positive=True)
-#         h = sy.Symbol('h', real=True)
-#         k = sy.Symbol('k', real=True)
-#         p = sy.Symbol('p', real=True)
-#         q= sy.Symbol('q', real=True)
-#         l = sy.Symbol('L', real=True)
-#     else :
-#         a = sy.Function('a', positive=True)(elementOf)
-#         h = sy.Function('h', real=True)(elementOf)
-#         k = sy.Function('k', real=True)(elementOf)
-#         p = sy.Function('p', real=True)(elementOf)
-#         q= sy.Function('q', real=True)(elementOf)
-#         l = sy.Function('L', real=True)(elementOf)
+def CreateSymbolicElements(elementOf = None) -> EquinoctialElements :
+    if(elementOf == None):
+        a = sy.Symbol('a', positive=True)
+        h = sy.Symbol('h', real=True)
+        k = sy.Symbol('k', real=True)
+        p = sy.Symbol('p', real=True)
+        q= sy.Symbol('q', real=True)
+        l = sy.Symbol('L', real=True)
+    else :
+        a = sy.Function('a', positive=True)(elementOf)
+        h = sy.Function('h', real=True)(elementOf)
+        k = sy.Function('k', real=True)(elementOf)
+        p = sy.Function('p', real=True)(elementOf)
+        q= sy.Function('q', real=True)(elementOf)
+        l = sy.Function('L', real=True)(elementOf)
 
-#     mu = sy.Symbol(r'\mu', positive=True)
-#     return EquinoctialElements(a, h, k, p, q, l, mu)
+    mu = sy.Symbol(r'\mu', positive=True)
+    return EquinoctialElements(a, h, k, p, q, l, mu)
 
