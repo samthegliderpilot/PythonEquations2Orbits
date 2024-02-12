Many fixes to scaling state variables and time

Tests/Problems/testPlanerLeoToGeoProblem.py
@@ -1,18 +1,15 @@
 import unittest
 from pyeq2orb.Problems.ContinuousThrustCircularOrbitTransfer import ContinuousThrustCircularOrbitTransferProblem
-from pyeq2orb.SymbolicOptimizerProblem import SymbolicProblem
 from scipy.integrate import solve_ivp # type: ignore
 import numpy as np
 import sympy as sy
-from pyeq2orb.SymbolicOptimizerProblem import SymbolicProblem
-from pyeq2orb.ScaledSymbolicProblem import ScaledSymbolicProblem
-from pyeq2orb.Problems.ContinuousThrustCircularOrbitTransfer import ContinuousThrustCircularOrbitTransferProblem
 from pyeq2orb.Numerical import ScipyCallbackCreators
 from pyeq2orb.Numerical.LambdifyHelpers import OdeLambdifyHelperWithBoundaryConditions
 import pyeq2orb as pe2o
 import importlib
 from typing import List
 from pyeq2orb.Symbolics.SymbolicUtilities import SafeSubs
+from pyeq2orb.ProblemBase import Problem, ProblemVariable
 
 class testPlanerLeoToGeoProblem(unittest.TestCase) :
     @staticmethod
@@ -42,24 +39,21 @@ class testPlanerLeoToGeoProblem(unittest.TestCase) :
             nus = [sy.Symbol('B_{u_f}'), sy.Symbol('B_{v_f}')]
         baseProblem = ContinuousThrustCircularOrbitTransferProblem()
         initialStateValues = baseProblem.CreateVariablesAtTime0(baseProblem.StateVariables)
-        problem = baseProblem #type: SymbolicProblem
-        lambdas = ScaledSymbolicProblem.CreateCoVector(problem.StateVariables, r'\lambda', problem.TimeSymbol)
-        baseProblem.StateVariables.extend(lambdas)
+        problem = baseProblem #type: Problem
+        lambdas = Problem.CreateCoVector(problem.StateVariables, r'\lambda', problem.TimeSymbol)
+        baseProblem.CostateSymbols.extend(lambdas)
         if scale :
-            newSvs = ScaledSymbolicProblem.CreateBarVariables(problem.StateVariables, problem.TimeSymbol) 
+            newSvs = Problem.CreateBarVariables(problem.StateVariables, problem.TimeSymbol) 
             scaleTimeFactor = None
             if scaleTime :
                 scaleTimeFactor = problem.TimeFinalSymbol
-            problem = baseProblem.ScaleProblem(newSvs, {problem.StateVariables[0]: initialStateValues[0], 
-                                                                problem.StateVariables[1]: initialStateValues[2], 
-                                                                problem.StateVariables[2]: initialStateValues[2], 
-                                                                problem.StateVariables[3]: 1.0,
-                                                                lambdas[0]: 1.0,
-                                                                lambdas[1]: 1.0,
-                                                                lambdas[2]: 1.0,
-                                                                lambdas[3]: 1.0} , scaleTimeFactor)
-            lambdas = problem.StateVariables[4:]
-            
+            problem = baseProblem.ScaleStateVariables(newSvs, {problem.StateVariables[0]: newSvs[0]*initialStateValues[0], 
+                                                                problem.StateVariables[1]: newSvs[1]*initialStateValues[2], 
+                                                                problem.StateVariables[2]: newSvs[2]* initialStateValues[2], 
+                                                                problem.StateVariables[3]: newSvs[3]})    
+            if scaleTime :
+                tau = sy.Symbol('\tau', real=True)
+                problem = problem.ScaleTime(tau, sy.Symbol('\tau_0', real=True), sy.Symbol('\tau_f', real=True), tau*problem.TimeFinalSymbol)   
 
         # make the time array
         tArray = np.linspace(0.0, tfOrg, 1200)
@@ -92,22 +86,21 @@ class testPlanerLeoToGeoProblem(unittest.TestCase) :
         # this next block does most of the problem, pretty standard optimal control actions
         #problem.Lambdas = 
         
-        hamiltonian = SymbolicProblem.CreateHamiltonianStatic(problem.StateVariables, problem.TimeSymbol, sy.Matrix([problem.StateVariableDynamics]).transpose(), 0, lambdas)
-        lambdaDotExpressions = SymbolicProblem.CreateLambdaDotConditionStatic(hamiltonian, sy.Matrix([problem.StateVariables]).transpose())
-        dHdu = SymbolicProblem.CreateHamiltonianControlExpressionsStatic(hamiltonian, sy.Matrix([problem.ControlVariables]).transpose())[0]
+        hamiltonian = Problem.CreateHamiltonianStatic(problem.TimeSymbol, sy.Matrix([problem.StateVariableDynamics]).transpose(), 0, lambdas)
+        lambdaDotExpressions = Problem.CreateLambdaDotConditionStatic(hamiltonian, sy.Matrix([problem.StateVariables]).transpose())
+        dHdu = Problem.CreateHamiltonianControlExpressionsStatic(hamiltonian, sy.Matrix([problem.ControlVariables]).transpose())[0]
         controlSolved = sy.solve(dHdu, problem.ControlVariables[0])[0] # something that may be different for other problems is when there are multiple control variables
 
         # you are in control of the order of integration variables and what EOM's get evaluated, start updating the problem
         # this line sets the lambdas in the equations of motion and integration state
         #problem.StateVariableDynamics.update(zip(lambdas, lambdaDotExpressions))
         for i in range(0, len(lambdas)) :
-            problem.StateVariableDynamics.append(lambdaDotExpressions[i])
-            #problem.StateVariables.append(lambdas[i])
+            problem.AddCostateVariable(ProblemVariable(lambdas[i], lambdaDotExpressions[i]))
         problem.SubstitutionDictionary[problem.ControlVariables[0]]  =controlSolved
         SafeSubs(problem.StateVariableDynamics, {problem.ControlVariables[0]: controlSolved})
         # the trig simplification needs the deep=True for this problem to make the equations even cleaner
         for i in range(0, len(problem.StateVariableDynamics)) :
-            problem.StateVariableDynamics[i] = problem.StateVariableDynamics[i].trigsimp(deep=True).simplify() # some simplification to make numerical code more stable later, and that is why this code forces us to do things somewhat manually.  There are often special things like this that we ought to do that you can't really automate.
+            problem._stateVariables[i].FirstOrderDynamics = problem._stateVariables[i].FirstOrderDynamics.trigsimp(deep=True).simplify() # some simplification to make numerical code more stable later, and that is why this code forces us to do things somewhat manually.  There are often special things like this that we ought to do that you can't really automate.
 
         ## Start with the boundary conditions
         if scaleTime : # add BC if we are working with the final time (kind of silly for this example, but we need an equal number of in's and out's for fsolve later)
@@ -129,6 +122,7 @@ class testPlanerLeoToGeoProblem(unittest.TestCase) :
         del problem.StateVariableDynamics[-1]
         del problem.StateVariables[-1]
         problem.BoundaryConditions.remove(transversalityCondition[-1])
+        problem._costateElements.pop()
         lmdTheta = lambdas.pop()
         constantsSubsDict[lmdTheta]=0
         constantsSubsDict[lmdTheta.subs(problem.TimeSymbol, problem.TimeFinalSymbol)]=0
@@ -144,7 +138,12 @@ class testPlanerLeoToGeoProblem(unittest.TestCase) :
         if len(nus) > 0 :
             otherArgs.extend(nus)
         
-        lambdifyHelper = OdeLambdifyHelperWithBoundaryConditions(problem.TimeSymbol, problem.TimeInitialSymbol, problem.TimeFinalSymbol, problem.StateVariables, problem.StateVariableDynamics, problem.BoundaryConditions, otherArgs, problem.SubstitutionDictionary)
+        allSvs = problem.StateVariables
+        allSvs.extend(problem.CostateSymbols)
+
+        allDynamics = problem.StateVariableDynamics
+        allDynamics.extend(problem.CostateDynamicsEquations)
+        lambdifyHelper = OdeLambdifyHelperWithBoundaryConditions(problem.TimeSymbol, problem.TimeInitialSymbol, problem.TimeFinalSymbol, allSvs, allDynamics, problem.BoundaryConditions, otherArgs, problem.SubstitutionDictionary)
         
         odeIntEomCallback = lambdifyHelper.CreateSimpleCallbackForSolveIvp()
         # run a test solution to get a better guess for the final nu values, this is a good technique, but 
@@ -164,7 +163,8 @@ class testPlanerLeoToGeoProblem(unittest.TestCase) :
             initialFSolveStateGuess[-2] = finalValues[5]
             initialFSolveStateGuess[-1] = finalValues[6]
 
-        fSolveCallback = ContinuousThrustCircularOrbitTransferProblem.createSolveIvpSingleShootingCallbackForFSolve(problem, problem.StateVariables, [r0, u0, v0, lon0], tArray, odeIntEomCallback, problem.BoundaryConditions, lambdas, otherArgs)
+
+        fSolveCallback = ContinuousThrustCircularOrbitTransferProblem.createSolveIvpSingleShootingCallbackForFSolve(problem, allSvs, [r0, u0, v0, lon0], tArray, odeIntEomCallback, problem.BoundaryConditions, lambdas, otherArgs)
         return (odeIntEomCallback, fSolveCallback, tArray, [r0, u0, v0, lon0], problem)
 
     def testInitialization(self) :
@@ -182,7 +182,7 @@ class testPlanerLeoToGeoProblem(unittest.TestCase) :
 
     def testDifferentialTransversalityCondition(self) :
         problem = ContinuousThrustCircularOrbitTransferProblem()
-        lambdas = SymbolicProblem.CreateCoVector(problem.StateVariables, 'L', problem.TimeFinalSymbol)
+        lambdas = Problem.CreateCoVector(problem.StateVariables, 'L', problem.TimeFinalSymbol)
         hamiltonian = problem.CreateHamiltonian(lambdas)
         xversality = problem.TransversalityConditionInTheDifferentialForm(hamiltonian, 0.0, lambdas) # not allowing final time to vary
 
@@ -202,7 +202,7 @@ class testPlanerLeoToGeoProblem(unittest.TestCase) :
             i=i+1
         odeAns = solve_ivp(odeSolveIvpCb, [tArray[0], tArray[-1]], [*z0, *knownAnswer], args=tuple([]), t_eval=tArray, dense_output=True, method="LSODA", rtol=1.49012e-8, atol=1.49012e-11)  
         finalState = ScipyCallbackCreators.GetFinalStateFromIntegratorResults(odeAns)
-        self.assertAlmostEqual(finalState[0], 42162071.898083754, delta=30, msg="radius check")
+        self.assertAlmostEqual(finalState[0], 42162071.898083754, delta=70, msg="radius check")
         self.assertAlmostEqual(finalState[1], 0.000, 2, msg="u check")
         self.assertAlmostEqual(finalState[2], 3074.735, 1, msg="v check")
 

Tests/testScaledSymbolicProblem.py
@@ -1,35 +1,24 @@
 import unittest
 import sympy as sy
-from pyeq2orb.ScaledSymbolicProblem import ScaledSymbolicProblem
-from pyeq2orb.ScaledSymbolicProblem import SymbolicProblem
+from collections import OrderedDict
 from pyeq2orb.Problems.OneDimensionalMinimalWorkProblem import OneDWorkSymbolicProblem
 from pyeq2orb.Problems.ContinuousThrustCircularOrbitTransfer import ContinuousThrustCircularOrbitTransferProblem
 from scipy.integrate import solve_ivp # type: ignore
 from pyeq2orb.Numerical import ScipyCallbackCreators
 from pyeq2orb import SafeSubs
+from pyeq2orb.ProblemBase import Problem, ProblemVariable
+
 
 class testScaledSymbolicProblem(unittest.TestCase) :
-
-    def testVariableScaling(self) :
-        baseProblem = OneDWorkSymbolicProblem()
-        newSvs = ScaledSymbolicProblem.CreateBarVariables(baseProblem.StateVariables, baseProblem.TimeSymbol)
-        scalingDict = {}
-        scalingDict[baseProblem.StateVariables[0]]=2
-        scalingDict[baseProblem.StateVariables[1]]=3 
-        outerProblem = baseProblem.ScaleProblem(newSvs, scalingDict)
-        firstEomValue = outerProblem.StateVariableDynamics[0].subs({outerProblem.StateVariables[0]: 1.5, outerProblem.StateVariables[1]: 0.4})
-        secondEomValue=outerProblem.StateVariableDynamics[1].subs({outerProblem.ControlVariables[0]: 1.6})
-        self.assertEqual(3.0*0.4/2.0, firstEomValue, msg="first eom evaluated")
-        self.assertEqual(1.6/3.0, secondEomValue, msg="second eom evaluated")  
 
     def testCreatingDifferentialTransversalityCondition(self) :
         orgProblem = ContinuousThrustCircularOrbitTransferProblem()
         mu = orgProblem.Mu
         t = orgProblem.TimeSymbol
         newSvs = [sy.Function('rs')(t), sy.Function('rs')(t), sy.Function('vs')(t), sy.Function('lons')(t)]
-        subs = {orgProblem.StateVariables[0]: 4.0, orgProblem.StateVariables[1]: 3.0, orgProblem.StateVariables[2]: 5.0, orgProblem.StateVariables[3]: 7.0, }
-        problem = orgProblem.ScaleProblem(newSvs, subs)
-        lambdas = SymbolicProblem.CreateCoVector(problem.StateVariables, 'L', problem.TimeFinalSymbol)
+        subs = {orgProblem.StateVariables[0]: 4.0*newSvs[0], orgProblem.StateVariables[1]: 3.0*newSvs[1], orgProblem.StateVariables[2]: 5.0*newSvs[2], orgProblem.StateVariables[3]: 7.0*newSvs[3] }
+        problem = orgProblem.ScaleStateVariables(newSvs, subs)
+        lambdas = Problem.CreateCoVector(problem.StateVariables, 'L', problem.TimeFinalSymbol)
         r = problem.StateVariables[0].subs(problem.TimeSymbol, problem.TimeFinalSymbol)
         l_r = lambdas[0]
         l_v = lambdas[2]
@@ -44,9 +33,9 @@ class testScaledSymbolicProblem(unittest.TestCase) :
         orgProblem = ContinuousThrustCircularOrbitTransferProblem()
         t = sy.Symbol('t')
         newSvs = [sy.Function('rs')(t), sy.Function('rs')(t), sy.Function('vs')(t), sy.Function('lons')(t)]
-        subs = {orgProblem.StateVariables[0]: 4.0, orgProblem.StateVariables[1]: 3.0, orgProblem.StateVariables[2]: 5.0, orgProblem.StateVariables[3]: 7.0, }
-        problem = orgProblem.ScaleProblem(newSvs, subs)
-        lambdas = SymbolicProblem.CreateCoVector(problem.StateVariables, 'l', problem.TimeFinalSymbol)
+        subs = {orgProblem.StateVariables[0]: 4.0*newSvs[0], orgProblem.StateVariables[1]: 3.0*newSvs[1], orgProblem.StateVariables[2]: 5.0*newSvs[2], orgProblem.StateVariables[3]: 7.0*newSvs[3] }
+        problem = orgProblem.ScaleStateVariables(newSvs, subs)
+        lambdas = Problem.CreateCoVector(problem.StateVariables, 'l', problem.TimeFinalSymbol)
         l_r = lambdas[0]
         l_u = lambdas[1]
         l_v = lambdas[2]
@@ -142,3 +131,252 @@ class testScaledSymbolicProblem(unittest.TestCase) :
         self.assertAlmostEqual(descaled[problem.WrappedProblem.StateVariables[0]][-1], 42162080.85814935, delta=50, msg="radius check descaled")
         self.assertAlmostEqual(descaled[problem.WrappedProblem.StateVariables[1]][-1], 0.000, 2, msg="u check descaled")
         self.assertAlmostEqual(descaled[problem.WrappedProblem.StateVariables[2]][-1], 3074.735, 1, msg="v check descaled")    
+
+
+    def testScalingStateVariables(self):
+        t = sy.Symbol('t', real=True)
+        r = sy.Function('r')(t)
+        u = sy.Function('u')(t)
+        v = sy.Function('v')(t)
+        l = sy.Function('l')(t)
+        m = sy.Function('m')(t)
+        
+        r0 = sy.Symbol('r_0')
+        u0 = sy.Symbol('u_0')
+        v0 = sy.Symbol('v_0')
+        l0 = sy.Symbol('l_0')
+
+        m0 = sy.Symbol('m_0', real=True, positive=True)
+        mDot =sy.Symbol('\dot{m}', real=True, negative=True)
+        mu = sy.Symbol(r'\mu', real=True)
+        thrust =sy.Symbol('T', real=True, positive=True)
+        angle = sy.Function(r'\alpha')(t)
+
+        rDot = u
+        uDot = v*v/r - mu/(r*r) + thrust*sy.sin(angle)/(m0-mDot*t)
+        vDot = -u*v/r + (thrust*sy.cos(angle))/(m0-mDot*t)
+        lDot = v/r
+
+        prob = Problem()
+        prob.TimeSymbol =t 
+        prob.Time0Symbol = sy.Symbol('t_0')
+        prob.TimeFinalSymbol = sy.Symbol('t_f')
+        prob.AddStateVariable(ProblemVariable(r, rDot))
+        prob.AddStateVariable(ProblemVariable(u, uDot))
+        prob.AddStateVariable(ProblemVariable(v, vDot))
+        prob.AddStateVariable(ProblemVariable(l, lDot))
+
+        prob.ControlVariables.append(angle)
+
+        prob.TerminalCost = r.subs(t, prob.TimeFinalSymbol)
+
+        tf = prob.TimeFinalSymbol
+
+        bc1 = u.subs(t, tf)
+        bc2 = v.subs(t, tf) - 3000
+        prob.BoundaryConditions.append(bc1)
+        prob.BoundaryConditions.append(bc2)
+
+        # start scaling it
+        rb = sy.Function(r'\bar{r}')(t)
+        ub = sy.Function(r'\bar{u}')(t)
+        vb = sy.Function(r'\bar{v}')(t)
+        lb = sy.Function(r'\bar{l}')(t)
+        newVariables = [rb, ub, vb, lb]
+        scalingDict = {r:r0*rb, u:v0*ub, v:v0*vb, l:lb}
+
+        #DO IT
+        scaledProblem = prob.ScaleStateVariables(newVariables, scalingDict)
+
+        # from the book (not scaling by time, so tf is 1 and t instead of \tau)
+        eta = v0/r0
+        simpleSubsDict = {r:rb*r0, u:ub*v0, v:vb*v0, l:lb*1}
+        expectedRDot = SafeSubs(rDot/r0, simpleSubsDict)
+        expectedUDot = SafeSubs(uDot/v0, simpleSubsDict)
+        expectedVDot = SafeSubs(vDot/v0, simpleSubsDict)
+        expectedLDot = SafeSubs(lDot/1, simpleSubsDict)
+
+        self.assertTrue((expectedRDot - scaledProblem.StateVariableDynamics[0]).is_zero, msg="rDot")
+        self.assertTrue((expectedUDot - scaledProblem.StateVariableDynamics[1]).is_zero, msg="uDot")
+        self.assertTrue((expectedVDot - scaledProblem.StateVariableDynamics[2]).is_zero, msg="vDot")        
+        self.assertTrue((expectedLDot - scaledProblem.StateVariableDynamics[3]).is_zero, msg="lDot")
+
+        bc1Expected = ub.subs(t, tf)*v0
+        bc2Expected = vb.subs(t, tf)*v0-3000
+        self.assertTrue((bc1Expected - scaledProblem.BoundaryConditions[0]).is_zero, msg="u_f bc")
+        self.assertTrue((bc2Expected - scaledProblem.BoundaryConditions[1]).is_zero, msg="v_f bc")
+
+        self.assertTrue((rb.subs(t, tf)-scaledProblem.TerminalCost).is_zero, msg="cost")        
+
+    def testScalingTime(self):
+        t = sy.Symbol('t', real=True)
+        r = sy.Function('r')(t)
+        u = sy.Function('u')(t)
+        v = sy.Function('v')(t)
+        l = sy.Function('l')(t)
+        m = sy.Function('m')(t)
+        
+        r0 = sy.Symbol('r_0')
+        u0 = sy.Symbol('u_0')
+        v0 = sy.Symbol('v_0')
+        l0 = sy.Symbol('l_0')
+
+        m0 = sy.Symbol('m_0', real=True, positive=True)
+        mDot =sy.Symbol('\dot{m}', real=True, negative=True)
+        mu = sy.Symbol(r'\mu', real=True)
+        thrust =sy.Symbol('T', real=True, positive=True)
+        angle = sy.Function(r'\alpha')(t)
+
+        rDot = u
+        uDot = v*v/r - mu/(r*r) + thrust*sy.sin(angle)/(m0-mDot*t)
+        vDot = -u*v/r + (thrust*sy.cos(angle))/(m0-mDot*t)
+        lDot = v/r
+
+        prob = Problem()
+        prob.TimeSymbol =t 
+        prob.TimeInitialSymbol = sy.Symbol('t_0', real=True)
+        prob.TimeFinalSymbol = sy.Symbol('t_f', real=True)
+        prob.AddStateVariable(ProblemVariable(r, rDot))
+        prob.AddStateVariable(ProblemVariable(u, uDot))
+        prob.AddStateVariable(ProblemVariable(v, vDot))
+        prob.AddStateVariable(ProblemVariable(l, lDot))
+
+        prob.ControlVariables.append(angle)
+
+        prob.TerminalCost = r.subs(t, prob.TimeFinalSymbol)
+
+        tf = prob.TimeFinalSymbol
+
+        bc1 = u.subs(t, tf)
+        bc2 = v.subs(t, tf) - 3000
+        prob.BoundaryConditions.append(bc1)
+        prob.BoundaryConditions.append(bc2)
+
+        tau = sy.Symbol('TT', real=True)
+        tau0 = sy.Symbol('TT_0', real=True)
+        tauF = sy.Symbol('TT_f', real=True)
+        tauInTermsOfT = tau * tf
+
+        rb = sy.Function(r'r')(tau)
+        ub = sy.Function(r'u')(tau)
+        vb = sy.Function(r'v')(tau)
+        lb = sy.Function(r'l')(tau)
+        newVariables = [rb, ub, vb, lb]
+        scalingDict = {}
+
+        #DO IT
+        scaledProblem = prob.ScaleTime(tau, tau0, tauF, tauInTermsOfT)
+
+        # from the book (not scaling by time)
+        eta = v0/r0
+        
+        simpleSubsDict =OrderedDict()
+        simpleSubsDict[r] = rb
+        simpleSubsDict[u] = ub
+        simpleSubsDict[v] = vb
+        simpleSubsDict[l] = lb
+        simpleSubsDict[t] = tau*tf
+        #simpleSubsDict[angle.subs(t, tau*tf)] = angle.subs(t, tau)
+        expectedRDot = SafeSubs(rDot*tf, simpleSubsDict)
+        expectedUDot = SafeSubs(uDot*tf, simpleSubsDict).subs(angle.subs(t, tau*tf), angle.subs(t, tau))
+        expectedVDot = SafeSubs(vDot*tf, simpleSubsDict).subs(angle.subs(t, tau*tf), angle.subs(t, tau))
+        expectedLDot = SafeSubs(lDot*tf, simpleSubsDict)
+
+        self.assertTrue((expectedRDot - scaledProblem.StateVariableDynamics[0]).is_zero, msg="rDot")
+        self.assertTrue((expectedUDot - scaledProblem.StateVariableDynamics[1]).is_zero, msg="uDot")
+        self.assertTrue((expectedVDot - scaledProblem.StateVariableDynamics[2]).is_zero, msg="vDot")        
+        self.assertTrue((expectedLDot - scaledProblem.StateVariableDynamics[3]).is_zero, msg="lDot")
+
+        bc1Expected = ub.subs(tau, tauF)
+        bc2Expected = vb.subs(tau, tauF)-3000
+        self.assertTrue((bc1Expected - scaledProblem.BoundaryConditions[0]).is_zero, msg="u_f bc")
+        self.assertTrue((bc2Expected - scaledProblem.BoundaryConditions[1]).is_zero, msg="v_f bc")
+
+        self.assertTrue((rb.subs(tau, tauF)-scaledProblem.TerminalCost).is_zero, msg="cost")           
+
+    def testScalingStateAndTime(self):
+        t = sy.Symbol('t', real=True)
+        r = sy.Function('r')(t)
+        u = sy.Function('u')(t)
+        v = sy.Function('v')(t)
+        l = sy.Function('l')(t)
+        m = sy.Function('m')(t)
+        
+        r0 = sy.Symbol('r_0')
+        u0 = sy.Symbol('u_0')
+        v0 = sy.Symbol('v_0')
+        l0 = sy.Symbol('l_0')
+
+        m0 = sy.Symbol('m_0', real=True, positive=True)
+        mDot =sy.Symbol('\dot{m}', real=True, negative=True)
+        mu = sy.Symbol(r'\mu', real=True)
+        thrust =sy.Symbol('T', real=True, positive=True)
+        angle = sy.Function(r'\alpha')(t)
+
+        rDot = u
+        uDot = v*v/r - mu/(r*r) + thrust*sy.sin(angle)/(m0-mDot*t)
+        vDot = -u*v/r + (thrust*sy.cos(angle))/(m0-mDot*t)
+        lDot = v/r
+
+        prob = Problem()
+        prob.TimeSymbol =t 
+        prob.TimeInitialSymbol = sy.Symbol('t_0', real=True)
+        prob.TimeFinalSymbol = sy.Symbol('t_f', real=True)
+        prob.AddStateVariable(ProblemVariable(r, rDot))
+        prob.AddStateVariable(ProblemVariable(u, uDot))
+        prob.AddStateVariable(ProblemVariable(v, vDot))
+        prob.AddStateVariable(ProblemVariable(l, lDot))
+
+        prob.ControlVariables.append(angle)
+        
+        tf = prob.TimeFinalSymbol
+
+        prob.TerminalCost = r.subs(t, prob.TimeFinalSymbol)
+
+        bc1 = u.subs(t, tf)
+        bc2 = v.subs(t, tf) - 3000
+        prob.BoundaryConditions.append(bc1)
+        prob.BoundaryConditions.append(bc2)
+
+        tau = sy.Symbol('TT', real=True)
+        tau0 = sy.Symbol('TT_0', real=True)
+        tauF = sy.Symbol('TT_f', real=True)
+        tauInTermsOfT = tau * tf
+
+        rb = sy.Function(r'\bar{r}')(tau)
+        ub = sy.Function(r'\bar{u}')(tau)
+        vb = sy.Function(r'\bar{v}')(tau)
+        lb = sy.Function(r'\bar{l}')(tau)
+        newVariables = [rb, ub, vb, lb]
+        scalingDict = {r:r0*rb, u:v0*ub, v:v0*vb, l:lb}
+
+        #DO IT
+        scaledStateProblem = prob.ScaleStateVariables(newVariables, scalingDict)
+        scaledProblem = scaledStateProblem.ScaleTime(tau, tau0, tauF, tauInTermsOfT)
+
+        # from the book (not scaling by time)
+        eta = v0/r0
+        
+        simpleSubsDict =OrderedDict()
+        simpleSubsDict[r] = rb*r0
+        simpleSubsDict[u] = ub*v0
+        simpleSubsDict[v] = vb*v0
+        simpleSubsDict[l] = lb*1
+        simpleSubsDict[t] = tau*tf
+        #simpleSubsDict[angle.subs(t, tau*tf)] = angle.subs(t, tau)
+        expectedRDot = SafeSubs(rDot*tf/r0, simpleSubsDict)
+        expectedUDot = SafeSubs(uDot*tf/v0, simpleSubsDict).subs(angle.subs(t, tau*tf), angle.subs(t, tau))
+        expectedVDot = SafeSubs(vDot*tf/v0, simpleSubsDict).subs(angle.subs(t, tau*tf), angle.subs(t, tau))
+        expectedLDot = SafeSubs(lDot*tf/1, simpleSubsDict)
+
+        self.assertTrue((expectedRDot - scaledProblem.StateVariableDynamics[0]).is_zero, msg="rDot")
+        self.assertTrue((expectedUDot - scaledProblem.StateVariableDynamics[1]).is_zero, msg="uDot")
+        self.assertTrue((expectedVDot - scaledProblem.StateVariableDynamics[2]).is_zero, msg="vDot")        
+        self.assertTrue((expectedLDot - scaledProblem.StateVariableDynamics[3]).is_zero, msg="lDot")
+
+        bc1Expected = ub.subs(tau, tauF)*v0
+        bc2Expected = vb.subs(tau, tauF)*v0-3000
+        self.assertTrue((bc1Expected - scaledProblem.BoundaryConditions[0]).is_zero, msg="u_f bc")
+        self.assertTrue((bc2Expected - scaledProblem.BoundaryConditions[1]).is_zero, msg="v_f bc")      
+
+        self.assertTrue((rb.subs(t, tf)-scaledProblem.TerminalCost).is_zero, msg="cost")             

pyeq2orb/DemosAndPrototypes/SepspotRecreation.py
@@ -13,9 +13,9 @@ from pyeq2orb.ForceModels.TwoBodyForce import CreateTwoBodyMotionMatrix, CreateT
 from pyeq2orb.Coordinates.CartesianModule import Cartesian, MotionCartesian
 from pyeq2orb.Coordinates.KeplerianModule import KeplerianElements
 import pyeq2orb.Coordinates.KeplerianModule as KepModule
+from pyeq2orb.ProblemBase import Problem, ProblemVariable
 import pyeq2orb.Coordinates.ModifiedEquinoctialElementsModule as mee
 from IPython.display import display
-from pyeq2orb.SymbolicOptimizerProblem import SymbolicProblem
 from pyeq2orb.Numerical.LambdifyHelpers import OdeLambdifyHelperWithBoundaryConditions
 import scipyPaperPrinter as jh #type: ignore
 from scipy.integrate import solve_ivp #type: ignore
@@ -60,29 +60,47 @@ for i in range(0, 3):
     fullSubsDictionary[fHatSy[i]] = rotMatrix.col(0)[i]
     fullSubsDictionary[gHatSy[i]] = rotMatrix.col(1)[i]
     fullSubsDictionary[wHatSy[i]] = rotMatrix.col(2)[i]
+
+initialKepElements = KeplerianElements(7000, 0.0, 28.5*math.pi/180.0, 0, 0, -2.274742851, muVal)
+initialModifiedEquiElements = mee.ConvertKeplerianToEquinoctial(initialKepElements)
+initialEquiElements = mee.EquinoctialElementsHalfITrueLongitude.FromModifiedEquinoctialElements(initialModifiedEquiElements)
+a0V = float(initialEquiElements.SemiMajorAxis)
+h0V = float(initialEquiElements.EccentricitySinTermH)
+k0V = float(initialEquiElements.EccentricityCosTermK)
+p0V = float(initialEquiElements.InclinationSinTermP)
+q0V = float(initialEquiElements.InclinationCosTermQ)
+lon0= float(initialEquiElements.TrueLongitude)
+#t0V = 2444239.0 * 86400
+
+afV = 42000
+hfV = 0.0
+kfV = 0.001
+pfV = math.tan((1*math.pi/180.0)/2)
+qfV = 0.0
+tfV = 0.0    
 #%%
-F = sy.Symbol('F', real=True)
-G = sy.Symbol('G')
+#F = sy.Symbol('F', real=True)
+#G = sy.Symbol('G')
 L = simpleBoringEquiElements.TrueLongitude
-r = a*G**2/(1+h*sy.sin(L)+k*sy.cos(L))
+# r = a*G**2/(1+h*sy.sin(L)+k*sy.cos(L))
 
-B = 1/(1+G)
+# B = 1/(1+G)
 
-cosL = (a/r)*((1-B*h**2)*sy.cos(F)+h*k*B*sy.sin(F)-k)
-sinL = (a/r)*(h*k*B*sy.cos(F)+(1-B*k**2)*sy.sin(F)-h)
-sf = h+(r/a)*((1-B*h**2)*sy.sin(L)-h*k*B*sy.cos(L))/(G)
-cf = k+(r/a)*((1-B*k**2)*sy.cos(L)-h*k*B*sy.sin(L))/(G)
-drdk_F = -a*cf
-drdk_L = -r*(2*a*k+r*sy.cos(L))/(a*G**2)
+# cosL = (a/r)*((1-B*h**2)*sy.cos(F)+h*k*B*sy.sin(F)-k)
+# sinL = (a/r)*(h*k*B*sy.cos(F)+(1-B*k**2)*sy.sin(F)-h)
+# sf = h+(r/a)*((1-B*h**2)*sy.sin(L)-h*k*B*sy.cos(L))/(G)
+# cf = k+(r/a)*((1-B*k**2)*sy.cos(L)-h*k*B*sy.sin(L))/(G)
+# drdk_F = -a*cf
+# drdk_L = -r*(2*a*k+r*sy.cos(L))/(a*G**2)
 
 
 
-jh.showEquation(r'\frac{dr}{dk}_F', (drdk_F/k).simplify())
-jh.showEquation(r'\frac{dr}{dk}_L', (drdk_L/k).simplify())
-jh.showEquation("s", (drdk_F - drdk_L).simplify().trigsimp(deep=True))
+# jh.showEquation(r'\frac{dr}{dk}_F', (drdk_F/k).simplify())
+# jh.showEquation(r'\frac{dr}{dk}_L', (drdk_L/k).simplify())
+# jh.showEquation("s", (drdk_F - drdk_L).simplify().trigsimp(deep=True))
 
-r_f = a*(1-k*cf-h*sf)
-jh.showEquation("s",(r-r_f).simplify())
+# r_f = a*(1-k*cf-h*sf)
+# jh.showEquation("s",(r-r_f).simplify())
 
 #%%
 
@@ -114,7 +132,7 @@ def CreatePerturbationMatrixWithTrueLongitude(eelm, subsDict : Dict[sy.Expr, Sym
     K = (1+p**2+q**2)
     n = eelm.NSy
     subsDict[n] = eelm.N
-    GExp = G
+    GExp = sy.sqrt(1-h*h-k*k)
     G = sy.Function("G")(h, k)
     subsDict[G] = GExp
 
@@ -269,16 +287,42 @@ lonDot = sy.Matrix([[0],[0],[0],[0],[0],[1]])*UnperturbedTrueLongitudeTimeDeriva
 
 r = simpleBoringEquiElements.ROverA * simpleBoringEquiElements.SemiMajorAxis
 jh.showEquation("r", r)
+problem = Problem()
+for (k,v) in fullSubsDictionary.items():
+    problem.SubstitutionDictionary[k] = v
+problem.TimeSymbol = t
+problem.TimeInitialSymbol = sy.Symbol('t_0', real=True)
+problem.TimeFinalSymbol = sy.Symbol('t_f', real=True)
+z0 = SafeSubs(z, {t: problem.TimeInitialSymbol})
+zF = SafeSubs(z, {t: problem.TimeFinalSymbol})
+
+problem.BoundaryConditions.append(zF[0]-afV)
+problem.BoundaryConditions.append(zF[1]-hfV)
+problem.BoundaryConditions.append(zF[2]-kfV)
+problem.BoundaryConditions.append(zF[3]-pfV)
+problem.BoundaryConditions.append(zF[4]-qfV)
+problem.BoundaryConditions.append(zF[5]*0)
 
 #%%
 zDot = B*uSy*accelSy + lonDot
-
 for i in range(0, 6):
     for j in range(0, 3):
         jh.showEquation("B_{" + str(i+1) +"," +str(j+1) + "}", B[i,j])
 # zDot = M*uSy*accelSy + sy.Matrix([[0,0,0,0,0,taDifeq]]).transpose()
 #%%
 
+
+
+for i in range(0, len(x)):
+    problem.AddStateVariable(ProblemVariable(x[i], zDot[i]))
+
+tau = sy.Symbol('tt')
+originalProblem = problem
+problem = problem.ScaleTime(tau, sy.Symbol('tt_0'), sy.Symbol('tt_f'), tau*problem.TimeFinalSymbol)
+
+zDot = problem.EquationsOfMotionInMatrixForm()
+x = sy.Matrix(problem.StateVariables)
+B = SafeSubs(B, {t: tau})
 def recurseArgs(someFunction, argsICareAbout, existingArgs) : 
     recursed = False
     if someFunction in argsICareAbout and not someFunction in existingArgs:
@@ -317,7 +361,7 @@ display(mFullSymbol)
 #xDot = g1Sy+ aSy*mFullSymbol*uSy
 #jh.printMarkdown("Filling in our Hamiltonian, we get the following expression for our optimal thrust direction:")
 #lambdas = sy.Matrix([[r'\lambda_{1}',r'\lambda_{2}',r'\lambda_{3}',r'\lambda_{4}',r'\lambda_{5}']]).transpose()
-lambdas = SymbolicProblem.CreateCoVector(x, r'\lambda', t)
+lambdas = Problem.CreateCoVector(x, r'\lambda', problem.TimeSymbol)
 #lambdasSymbol = sy.Symbol(r'\lambda^T', commutative=False)
 hamiltonian = lambdas.transpose()*zDot
 # print(hamiltonian)
@@ -355,7 +399,7 @@ for expr in lmdDotArray:
     break
 
 # now we try to integrate
-#%%
+
 accelVal = 9.8e-5
 fullSubsDictionary[accelSy] = accelVal
 fullSubsDictionary[mu]=muVal
@@ -378,56 +422,26 @@ for k,v in fullSubsDictionary.items() :
 fullSubsDictionary = actualSubsDic
 
 
-
-
-initialKepElements = KeplerianElements(7000, 0.0, 28.5*math.pi/180.0, 0, 0, -2.274742851, muVal)
-initialModifiedEquiElements = mee.ConvertKeplerianToEquinoctial(initialKepElements)
-initialEquiElements = mee.EquinoctialElementsHalfITrueLongitude.FromModifiedEquinoctialElements(initialModifiedEquiElements)
-a0V = float(initialEquiElements.SemiMajorAxis)
-h0V = float(initialEquiElements.EccentricitySinTermH)
-k0V = float(initialEquiElements.EccentricityCosTermK)
-p0V = float(initialEquiElements.InclinationSinTermP)
-q0V = float(initialEquiElements.InclinationCosTermQ)
-lon0= float(initialEquiElements.TrueLongitude)
-#t0V = 2444239.0 * 86400
-
-afV = 42000
-hfV = 0.0
-kfV = 0.001
-pfV = math.tan((1*math.pi/180.0)/2)
-qfV = 0.0
-tfV = 0.0
-
-
-
 #%%
 
 
-problem = SymbolicProblem()
-problem.TimeSymbol = t
-problem.TimeInitialSymbol = sy.Symbol('t_0', real=True)
-problem.TimeFinalSymbol = sy.Symbol('t_f', real=True)
-z0 = SafeSubs(z, {t: problem.TimeInitialSymbol})
-zF = SafeSubs(z, {t: problem.TimeFinalSymbol})
-problem.StateVariables.extend(x)
-problem.StateVariables.extend(lambdas)
-problem.StateVariableDynamics.extend(zDot)
-problem.StateVariableDynamics.extend(lmdDotArray)
 
-problem.BoundaryConditions.append(zF[0]-afV)
-problem.BoundaryConditions.append(zF[1]-hfV)
-problem.BoundaryConditions.append(zF[2]-kfV)
-problem.BoundaryConditions.append(zF[3]-pfV)
-problem.BoundaryConditions.append(zF[4]-qfV)
-problem.BoundaryConditions.append(zF[5]*0)
+
+for i in range(0, len(lambdas)):
+    problem.AddCostateVariable(ProblemVariable(lambdas[i], lmdDotArray[i]))
+# problem.StateVariables.extend(x)
+# problem.StateVariables.extend(lambdas)
+# problem.StateVariableDynamics.extend(zDot)
+# problem.StateVariableDynamics.extend(lmdDotArray)
+
+
 for (k,v) in fullSubsDictionary.items():
     problem.SubstitutionDictionary[k] =v
 
 scaleDict = {} #type: Dict[sy.Symbol, SymbolOrNumber]
 for sv in problem.StateVariables :
     scaleDict[sv] = 1.0
-originalProblem = problem
-problem = problem.ScaleProblem(problem.StateVariables, scaleDict, problem.TimeFinalSymbol)
+
 
 lmdHelper = OdeLambdifyHelperWithBoundaryConditions.CreateFromProblem(problem)
 
@@ -444,11 +458,11 @@ lmdHelper.SubstitutionDictionary[originalProblem.TimeFinalSymbol] = originalProb
 # lmdHelper.BoundaryConditionExpressions.append(zF[4]-qfV)
 # lmdHelper.BoundaryConditionExpressions.append(zF[5]*0)
 lmdHelper.SymbolsToSolveForWithBoundaryConditions.append(z0[5].subs(originalProblem.TimeInitialSymbol, lmdHelper.t0))
-lmdHelper.SymbolsToSolveForWithBoundaryConditions.append(lambdas[0].subs(t, lmdHelper.t0))
-lmdHelper.SymbolsToSolveForWithBoundaryConditions.append(lambdas[1].subs(t, lmdHelper.t0))
-lmdHelper.SymbolsToSolveForWithBoundaryConditions.append(lambdas[2].subs(t, lmdHelper.t0))
-lmdHelper.SymbolsToSolveForWithBoundaryConditions.append(lambdas[3].subs(t, lmdHelper.t0))
-lmdHelper.SymbolsToSolveForWithBoundaryConditions.append(lambdas[4].subs(t, lmdHelper.t0))
+lmdHelper.SymbolsToSolveForWithBoundaryConditions.append(lambdas[0].subs(tau, lmdHelper.t0))
+lmdHelper.SymbolsToSolveForWithBoundaryConditions.append(lambdas[1].subs(tau, lmdHelper.t0))
+lmdHelper.SymbolsToSolveForWithBoundaryConditions.append(lambdas[2].subs(tau, lmdHelper.t0))
+lmdHelper.SymbolsToSolveForWithBoundaryConditions.append(lambdas[3].subs(tau, lmdHelper.t0))
+lmdHelper.SymbolsToSolveForWithBoundaryConditions.append(lambdas[4].subs(tau, lmdHelper.t0))
 #lmdHelper.SymbolsToSolveForWithBoundaryConditions.append(lambdas[5].subs(t, lmdHelper.t0))
 #lmdHelper.OtherArguments.append(lambdas[5].subs(t, lmdHelper.t0))
 # #%%
@@ -462,9 +476,10 @@ lmdHelper.SymbolsToSolveForWithBoundaryConditions.append(lambdas[4].subs(t, lmdH
 
 lmdGuess = [4.675229762, 5.413413947e2, -9.202702084e3, 1.778011878e1, -2.268455855e4, -2.274742851]#-2.2747428]
 #lmdGuess = [4.675229762, 8.413413947e2, -9.202702084e3, 1.778011878e1, -2.260455855e4, -2.2747428]
-fullInitialState = [a0V, h0V, k0V, p0V, q0V]
+fullInitialState = [a0V, h0V, k0V, p0V, q0V, lon0]
 fullInitialState.extend(lmdGuess)
 print("read to lambdify")
+
 #%%
 tArray = np.linspace(0.0, 1.0, 800)
 initialState = [a0V, h0V,k0V, p0V, q0V, lon0 ]
@@ -476,7 +491,7 @@ from pyeq2orb.Numerical import ScipyCallbackCreators #type: ignore
 ipvCallback = lmdHelper.CreateSimpleCallbackForSolveIvp()
 def realIpvCallback(initialStateInCb) :
     #print(initialStateInCb)
-    solution = solve_ivp(ipvCallback, [tArray[0], tArray[-1]], initialStateInCb, t_eval=tArray, dense_output=True, method="LSODA", rtol=1.49012e-8, atol=1.49012e-11)
+    solution = solve_ivp(ipvCallback, [tArray[0], tArray[-1]], initialStateInCb[0:-1], args=(initialStateInCb[-1],), t_eval=tArray, dense_output=True, method="LSODA", rtol=1.49012e-8, atol=1.49012e-11)
     solutionDictionary = ScipyCallbackCreators.ConvertEitherIntegratorResultsToDictionary(lmdHelper.NonTimeLambdifyArguments, solution)
     return solution
 
@@ -487,7 +502,7 @@ solverCb = lmdHelper.createCallbackToSolveForBoundaryConditions(realIpvCallback,
 #print(ipvCallback(0, [r0, u0, v0, lon0, 1.0, 0.001, 0.001, 0.0]))
 #solution = solve_ivp(ipvCallback, [tArray[0], tArray[-1]], initialState, t_eval=tArray, dense_output=True, method="LSODA", rtol=1.49012e-8, atol=1.49012e-11)
 
-fSolveSol = fsolve(solverCb, initialState[5:12], epsfcn=0.0001, full_output=True, factor=0.01)
+fSolveSol = fsolve(solverCb, initialState[5:11], epsfcn=0.0001, full_output=True, factor=0.01)
 
 print(fSolveSol)
 
@@ -563,7 +578,6 @@ for i in range(0, 6):
         jh.showEquation(z[i], (solution.y[i][-1]*180/math.pi)%360)
     else:
         jh.showEquation(z[i], solution.y[i][-1])
-#%%
 
 
 import plotly.graph_objects as go #type: ignore
@@ -585,3 +599,5 @@ fig.update_layout(margin=dict(l=0, r=0, b=0, t=30))
 fig['layout']['sliders'][0]['pad']=dict(r= 0, t= 0, b=0, l=0)
 fig['layout']['updatemenus'][0]['pad']=dict(r= 0, t= 0, b=0, l=0)
 fig.show()  
+
+